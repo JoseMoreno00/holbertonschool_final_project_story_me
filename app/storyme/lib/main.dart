2843763657ac336048be31d80c3a7975a07f6f68
mabreu88 <martymalkav7@gmail.com>
2024-01-25 00:50:14 -0300

    add flutter app files

--- app/storyme/lib/main.dart
+++ app/storyme/lib/main.dart
@@ -78,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
+        //  THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
