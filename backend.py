2b43c1fb00933711d8d065c82639be2118a8947a
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-09 18:10:31 -0800

    I hope in I

--- backend.py
+++ backend.py
@@ -1,28 +1,50 @@
 #!/usr/bin/python3
 from leonardo_api import Leonardo
+import requests
+import firebase_admin
+from firebase_admin import credentials, db
+"""
 
-leonardo = Leonardo(auth_token='eb36c72b-8bc7-4da4-bd40-bd90664c07a5')
+"""
 
-response = leonardo.post_generations(prompt="Little pig building a hay house.8bit", num_images=1,
-                                           width=1024, height=768,
-                                           guidance_scale=7)
 
-idimage = leonardo.wait_for_image_generation(generation_id=response['sdGenerationJob']['generationId'])
+c = credentials.Certificate("credentials.json")
+database = firebase_admin.initialize_app(
+    c, {"databaseURL": "https://storyme-dcd11-default-rtdb.firebaseio.com/"})
 
-postapi = []
-with open('leonardo_async.log', 'r') as filename:
-    for line in filename:
-        postapi.append(line)
-    postapi.reverse()
-    while 1:
-        if len(postapi) != 1:
-            postapi.pop()
-        else:
-            break
-    print(postapi)
-    alpha = postapi[0]
-    betta = alpha.split("{'generated_images': [{'url': ")
-    charly = betta[1]
-    delta = charly.split(", 'nsfw': False")
-    url = delta[0]
-    print(url)
+
+class APILEONARDO():
+
+    def __init__(self) -> None:
+        self.leonardo = Leonardo(auth_token='eb36c72b-8bc7-4da4-bd40-bd90664c07a5')
+
+    def request_ia(self, book, style):
+        refdb = db.reference(f"/promp/{style}/{book}")
+        promps = refdb.get()
+        for key, values in promps.items():
+            print(values)
+            response = self.leonardo.post_generations(prompt=f"{values}", num_images=1,
+                                            width=1024, height=768,
+                                            guidance_scale=7)
+            self.leonardo.wait_for_image_generation(generation_id=response['sdGenerationJob']['generationId'])
+            postapi = []
+            with open('leonardo_async.log', 'r') as filename:
+                for line in filename:
+                    postapi.append(line)
+                postapi.reverse()
+                while 1:
+                    if len(postapi) != 1:
+                        postapi.pop()
+                    else:
+                        break
+                alpha = postapi[0]
+                betta = alpha.split("{'generated_images': [{'url': '")
+                charly = betta[1]
+                delta = charly.split("', 'nsfw': False")
+                url = delta[0]
+                respuesta = requests.get(url)
+                with open(f"{key}-Image{book}-{style}.png", 'wb') as archivo:
+                    archivo.write(respuesta.content)
+
+# apireq = APILEONARDO()
+# apireq.request_ia('3_little_pigs_PM', 'anime')
