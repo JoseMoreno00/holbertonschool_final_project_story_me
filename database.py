5c9f4d3b49936f92a487eb5bddc476967351c193
Martín Abreu <martymalkav7@gmail.com>
2024-03-04 19:31:40 -0300

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import firebase_admin
-from firebase_admin import db, credentials
-
-cred = credentials.Certificate("credentials.json")
-firebase = firebase_admin.initialize_app(cred, {"databaseURL":"https://storyme-dcd11-default-rtdb.firebaseio.com/"})
-
